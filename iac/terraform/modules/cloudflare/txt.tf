20675a91f66b362e96c5f91155ba5d5ad6807be0
Ronald Carvalho <r@ronaldmiranda.com.br>
2022-05-30 15:41:03 -0300

    To review later (#6)
    * remove old applications structure
    
    * Update teleport agent to 9.3.0

--- iac/terraform/modules/cloudflare/txt.tf
+++ iac/terraform/modules/cloudflare/txt.tf
@@ -1,8 +1,9 @@
 resource "cloudflare_record" "txt" {
-  for_each = { for k in compact([for k, v in var.txt_records : v.enabled ? k : ""]) : k => var.txt_records[k] }
-  zone_id  = var.zone_id
-  name     = each.value.name
-  value    = each.value.value
-  type     = "TXT"
-  ttl      = 300
+  for_each        = { for k in compact([for k, v in var.txt_records : v.enabled ? k : ""]) : k => var.txt_records[k] }
+  zone_id         = var.zone_id
+  name            = each.value.name
+  value           = each.value.value
+  type            = "TXT"
+  ttl             = try(each.value.ttl, 300)
+  allow_overwrite = true
 }
