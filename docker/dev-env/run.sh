e00016a2e38a5580c9de8e57069576f4543270fd
Ronald Carvalho <ronald.carvalho@liferay.com>
2022-07-03 13:18:49 -0300

    Select dinamically cpu architecture for development environment.

--- docker/dev-env/run.sh
+++ docker/dev-env/run.sh
@@ -12,6 +12,23 @@ BUILD_DOCKER_TAG=$(git log -n 1 --pretty='format:%cd-%h' --date=format:'%Y%m%d%H
 CONTAINER_NAME='devenv-personal'
 ZSH_CONFIGURE=${ZSH_CONFIGURE:-"n"}
 
+_checkCPUPlatform() {
+  case $(uname -m) in
+    aarch64)
+      DOCKER_IMAGE=$DOCKER_ARM_IMAGE
+      ARCH='arm64';;
+    arm64)
+      DOCKER_IMAGE=$DOCKER_ARM_IMAGE
+      ARCH='arm64';;
+    x86_64)
+      DOCKER_IMAGE=$DOCKER_IMAGE
+      ARCH='amd64';;
+    amd64)
+      DOCKER_IMAGE=$DOCKER_IMAGE
+      ARCH='amd64';;
+  esac
+}
+
 build(){
   if [[ $(uname -m) == 'aarch64' ]] || [[ $(uname -m) == 'arm64' ]]; then
     docker build -t $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG \
@@ -48,17 +65,17 @@ socatRun() {
 }
 
 run(){
+  _checkCPUPlatform
+
   if [[ $(uname) == "Darwin" ]]; then
     GRP_CMD='useradd -ms /bin/zsh -d /home/$user -u $uid -g $gid $user'
-     [[ ! $(uname -m) == "aarch64" ]] && ARCH='arm64' || ARCH='amd64'
   else
     GRP_CMD='groupadd -g $gid $user && useradd -ms /bin/zsh -d /home/$user -u $uid -g $gid $user'
-    ARCH='amd64'
   fi
 
   docker build -t local:1000 -f dockerfile \
     --build-arg groupcmd="$GRP_CMD" \
-    --build-arg image=$DOCKER_ARM_IMAGE \
+    --build-arg image=$DOCKER_IMAGE \
     --build-arg user=$USER \
     --build-arg arch=$ARCH \
     --build-arg uid=$(id -u) \
