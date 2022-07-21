e34ec480d76085d494c7d9e38c451487e6c4c655
Ronald Carvalho <ronald.carvalho@liferay.com>
2022-07-20 23:48:57 -0300

    Add ARGOCD_ENV_ environment variable.

--- tools/argo/helm-resolver.sh
+++ tools/argo/helm-resolver.sh
@@ -2,10 +2,10 @@
 
 declare -a args=(
   ''$ARGOCD_APP_NAME''
-  ''$REPO_NAME'/'$CHART_NAME''
-  ''$VALUE_FILES''
-  '--version '$CHART_VERSION''
-  '--namespace '$NAMESPACE''
+  ''$ARGOCD_ENV_REPO_NAME'/'$ARGOCD_ENV_CHART_NAME''
+  ''$ARGOCD_ENV_VALUE_FILES''
+  '--version '$ARGOCD_ENV_CHART_VERSION''
+  '--namespace '$ARGOCD_ENV_NAMESPACE''
 )
 
 helm template ${args[*]}
