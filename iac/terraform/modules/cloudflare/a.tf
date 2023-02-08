e398d56aa817b544ebb9eb3d656d53e69465dde6
Ronald Carvalho <r@ronaldmiranda.com.br>
2023-02-08 13:13:33 -0300

    melhor gerenciamento de generators, utilizando modulo remoto do cloudflare (#20)

--- iac/terraform/modules/cloudflare/a.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "a" {
-  for_each = { for k in compact([for k, v in var.a_records : v.enabled ? k : ""]) : k => var.a_records[k] }
-  zone_id  = var.zone_id
-  name     = each.value.name
-  value    = each.value.value
-  type     = "A"
-  ttl      = each.value.ttl
-}
--- iac/terraform/modules/cloudflare/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "cloudflare" {
-  email   = var.CLOUDFLARE_EMAIL
-  api_key = var.CLOUDFLARE_API_KEY
-}
