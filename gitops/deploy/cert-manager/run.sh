b55c65d8ed08b4bfecfffa6ca450e89f507d166e
Ronald Carvalho <ronald.carvalho@liferay.com>
2023-03-12 23:48:02 -0300

    cert-manager installer

---
+++ gitops/deploy/cert-manager/run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+KUBE_CONTEXT=${3:-$(kubectl config current-context)}
+DRY_RUN=${DRY_RUN:-"y"}
+
+if [[ "$DRY_RUN" =~ ^([yY])+$ ]]; then
+  DRY_RUN="--dry-run"
+else
+  DRY_RUN=""
+fi
+
+helmReleaseName="cert-manager"
+helmChartRepo='../../stable/cert-manager'
+chartNamespace="cert-manager"
+
+command="install"
+
+declare -a args=(
+  $helmReleaseName
+  $helmChartRepo
+  '--create-namespace'
+  '--debug'
+  '--kube-context='$KUBE_CONTEXT''
+  '--wait'
+  '-n='$chartNamespace''
+  $DRY_RUN
+)
+
+if [[ $(helm list -n $chartNamespace -o json | \
+    jq -r '.[] | select(.name=="'$helmReleaseName'") | true') == true ]];then
+  command="upgrade"
+  args+=('--install' '--reset-values')
+else
+  args+=('--replace')
+fi
+
+echo "Installing/Upgrading Helm Chart ${helmReleaseName} in ${KUBE_CONTEXT} Context Kubernetes"
+helm $command ${args[*]}
