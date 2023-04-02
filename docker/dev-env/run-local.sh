9d77d0537ea5f89bbe0466d0ed8551e2649759da
Ronald Carvalho <r@ronaldmiranda.com.br>
2023-04-02 13:46:23 -0300

    Docker multi build (#25)
    * Add multi-platform docker build
    
    * change image to alpine, multi-arch build enhancements
    
    * back to ubuntu, alpine doesnt works well with op cli
    
    * remove unecessary cpu check
    
    * fix permissions by bypassing socket permissions on socat (ugly fix)
    
    * remove unused packages, fix build push command
    
    * Add docker image, dont specify targetarch to parallelize multi-arch builds
    
    ---------
    
    Co-authored-by: ronaldmiranda <ronaldmiranda r@ronaldmiranda.com.br>

--- docker/dev-env/run-local.sh
+++ docker/dev-env/run-local.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 STABLE_DOCKER_TAG=$(git log -n 1 --pretty='format:%cd-%h' --date=format:'%Y%m%d%H%M')
 DOCKER_REPOSITORY="gcr.io/ronaldmiranda/dev-env"
 DOCKER_IMAGE=$DOCKER_REPOSITORY:$STABLE_DOCKER_TAG
-DOCKER_ARM_IMAGE=$DOCKER_REPOSITORY:$STABLE_DOCKER_TAG
 DEVENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DEVENV_DIR/run.sh build-run
--- docker/dev-env/run.sh
+++ docker/dev-env/run.sh
@@ -5,66 +5,41 @@ set -euo pipefail
 COMMAND=${1:-"run"}
 
 DOCKER_REPOSITORY="gcr.io/ronaldmiranda/dev-env"
-DOCKER_IMAGE=${DOCKER_IMAGE:-"$DOCKER_REPOSITORY:202303190257-bbc0035"}
-DOCKER_ARM_IMAGE=${DOCKER_ARM_IMAGE:-"$DOCKER_REPOSITORY:202303190054-e6c362f"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"$DOCKER_REPOSITORY:202303280211-137e646"}
 BUILD_DOCKER_TAG=$(git log -n 1 --pretty='format:%cd-%h' --date=format:'%Y%m%d%H%M')
 
 CONTAINER_NAME='devenv-personal'
 DEVENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZSH_CONFIGURE=${ZSH_CONFIGURE:-"n"}
 
-_checkCPUPlatform() {
-  case $(uname -m) in
-  aarch64)
-    DOCKER_IMAGE=$DOCKER_ARM_IMAGE
-    ARCH='arm64'
-    ;;
-  arm64)
-    DOCKER_IMAGE=$DOCKER_ARM_IMAGE
-    ARCH='arm64'
-    ;;
-  x86_64)
-    DOCKER_IMAGE=$DOCKER_IMAGE
-    ARCH='amd64'
-    ;;
-  amd64)
-    DOCKER_IMAGE=$DOCKER_IMAGE
-    ARCH='amd64'
-    ;;
-  esac
-}
-
 build() {
-  if [[ $(uname -m) == 'aarch64' ]] || [[ $(uname -m) == 'arm64' ]]; then
-    docker build -t $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG \
-      -f $DEVENV_DIR/dockerfile.arm.base \
-      $DEVENV_DIR
-  else
-    docker build -t $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG -f $DEVENV_DIR/dockerfile.base \
-      $DEVENV_DIR
-  fi
+  docker build -t $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG -f $DEVENV_DIR/dockerfile.base \
+    $DEVENV_DIR
 }
 
 push() {
-  docker push $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG
+  docker buildx build --platform linux/arm64/v8,linux/amd64 -f $DEVENV_DIR/dockerfile.base \
+    --push -t $DOCKER_REPOSITORY:$BUILD_DOCKER_TAG $DEVENV_DIR
 }
 
 run() {
-  _checkCPUPlatform
+  SYSTEM=$(uname)
 
-  if [[ $(uname) == "Darwin" ]]; then
+  if [[ $SYSTEM == "Darwin" ]]; then
     GRP_CMD='useradd -ms /bin/zsh -d /home/$user -u $uid -g $gid $user'
+    WORK_DIR="/home/$USER"
   else
     GRP_CMD='groupadd -g $gid $user && useradd -ms /bin/zsh -d /home/$user -u $uid -g $gid $user'
+    WORK_DIR="$(pwd)"
   fi
 
   docker build -t local:1000 -f $DEVENV_DIR/dockerfile \
     --build-arg groupcmd="$GRP_CMD" \
     --build-arg image=$DOCKER_IMAGE \
     --build-arg user=$USER \
-    --build-arg arch=$ARCH \
     --build-arg uid=$(id -u) \
     --build-arg gid=$(id -g) \
+    --build-arg workdir=$WORK_DIR \
     $DEVENV_DIR
 
   declare -a args=(
@@ -72,6 +47,7 @@ run() {
     '--rm'
     '-e USR='$USER''
     '-e ZSH_CONFIGURE='$ZSH_CONFIGURE''
+    '-e HOST_PLATFORM='$SYSTEM''
     '-it'
     '-v dev-env-pessoal:/home/'$USER''
     '-v '$HOME':/dojo/identity'
@@ -79,14 +55,15 @@ run() {
     '--privileged'
   )
 
-  if [[ "$(uname)" = "Darwin" ]]; then
+  if [[ "$SYSTEM" = "Darwin" ]]; then
     args+=(
       '-u '$(id -u $USER):$(id -g $USER)''
       '-e TZ='$(ls -la /etc/localtime | cut -d/ -f8-9)''
-      '-v /var/run/docker.sock:/var/run/docker.sock'
+      # '-v /var/run/docker.sock:/var/run/docker.sock'
     )
+    socat TCP-LISTEN:12346,reuseaddr,fork,bind=192.168.205.1 UNIX-CLIENT:$HOME/.rd/docker.sock &
     socat TCP-LISTEN:12345,reuseaddr,fork,bind=192.168.205.1 UNIX-CLIENT:$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock &
-  elif [[ "$(uname)" = "Linux" ]]; then
+  elif [[ "$SYSTEM" = "Linux" ]]; then
     args+=(
       '-v /var/run/docker.sock:/var/run/docker.sock'
       '-v '$(realpath /etc/localtime)':/etc/localtime:ro'
@@ -103,10 +80,6 @@ build)
   build
   ;;
 push)
-  push
-  ;;
-build-push)
-  build
   push
   ;;
 build-run)
